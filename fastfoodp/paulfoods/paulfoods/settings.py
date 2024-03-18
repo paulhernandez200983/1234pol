86d6aac575fbccc2c29b6439453d2a04f778c07e
paulhernandez200983 <89218294+paulhernandez200983@users.noreply.github.com>
2024-03-18 14:44:32 -0600

    Update settings.py

--- fastfoodp/paulfoods/paulfoods/settings.py
+++ fastfoodp/paulfoods/paulfoods/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT =PurePath.joinpath(BASE_DIR, 'staticfiles'  )
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
